0a3b0c1eaff9cfe6bfdfd64a3f7f6c809ae783b4
Daniel Hocking <djhocking@gmail.com>
2022-01-02 20:58:04 -0500

    updating readme

---
+++ R/plot.R
@@ -0,0 +1,11 @@
+#' plot_weekly
+#'
+#' @param data data frame
+#'
+#' @return
+#' @export
+#'
+#' @examples
+plot_weekly <- function(data) {
+  if(is.null(data)) stop("data missing with no default")
+}
